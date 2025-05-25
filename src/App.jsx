be43734ee63b100b9c22492ecc27a691093f02f6
shankarjatin <shankarjatin1005@gmail.com>
2025-05-25 07:09:48 +0530

    setup done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+<div className="min-h-screen bg-gradient-to-tr from-purple-300 to-pink-200 flex items-center justify-center p-4">
+<h2>heello</h2>
+    </div>
+    </>
+  )
+}
+
+export default App
